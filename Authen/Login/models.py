07e268db4272e928ad7464a7d93c96fabbed8ef4
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-17 01:09:45 +0700

    test staticfiles

--- Authen/Login/models.py
+++ Authen/Login/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -9,3 +10,11 @@ class Course(models.Model):
     def __str__(self):
         return self.name
     
+class Student(AbstractUser):
+    sex_choice = ((0, "Nữ"), (1, "Nam"))
+    age = models.IntegerField(default=0)
+    sex = models.IntegerField(choices=sex_choice, default=0)
+    faculty = models.CharField(default='', max_length=100)
+    GPA = models.FloatField(default=0)
+
+
