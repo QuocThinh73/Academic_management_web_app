5849434417d4c262d0047f7ec9f81ff025121f53
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-17 00:55:07 +0700

    phan quyen user

--- Authen/Login/urls.py
+++ Authen/Login/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 from . import views
 
+app_name='Login'
 urlpatterns = [
     path('', views.IndexClass.as_view(), name='index'),
     path('Login/', views.LoginClass.as_view(), name='login'),
-    path('viewuser/', views.ViewUser.as_view(), name='viewuser')
+    path('viewuser/', views.ViewUser.as_view(), name='viewuser'),
+    path('viewscore/', views.view_score, name='viewscore'),
+    path('add_post/', views.AddPost.as_view(), name='add_post')
 ]
