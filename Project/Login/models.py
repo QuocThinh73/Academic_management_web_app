bdf95c43779d305eb6b35252b492104f93af2d5d
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-18 11:09:50 +0700

    Tao StudentUser va TeacherUser

--- Project/Login/models.py
+++ Project/Login/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class StudentUser(AbstractUser):
-    user_type = "Student"
+class MyUser(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
 
-class TeacherUser(AbstractUser):
-    user_type = "Teacher"
+class StudentUser(MyUser):
+    student_id = models.CharField(max_length=30)
+    is_student = True
+
+class TeacherUser(MyUser):
+    teacher_id = models.CharField(max_length=30)
+    is_teacher = True
--- Project/Login/views.py
+++ Project/Login/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "login/home.html")
+
+    def post(self, request):
+        return render(request, "login/login.html")
+    
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login/login.html")
