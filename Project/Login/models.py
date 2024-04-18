43d3d19da600dc01a056df048b005d37023232ee
trankiet2004 <kiet111104@gmail.com>
2024-04-18 20:06:29 +0700

    update lai file sqlite

--- Project/Login/models.py
+++ Project/Login/models.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
     USER_TYPE_CHOICES = [
-         ('Student', "Student"),
+         ('Student', 'Student'),
          ('Teacher', 'Teacher'),
-     ] 
+     ]
     user_type = models.CharField(max_length = 20, choices = USER_TYPE_CHOICES, default = "Student")
     
 
 class StudentUser(MyUser):
-    student_id = models.CharField(max_length=30)
+    student_id = models.CharField(max_length=30, default='default_value')
     score = models.FloatField(default = 0.0)
     date_of_birth = models.DateField(null = True, blank = True)
     major = models.CharField(max_length = 50, null = True, blank = True)
     enrollment_date = models.DateField(null = True, blank = True)
 
 class TeacherUser(MyUser):
-    teacher_id = models.CharField(max_length=30)
+    teacher_id = models.CharField(max_length=30, default='default_value')
     department = models.CharField(max_length = 30, null = True, blank = True)
 
