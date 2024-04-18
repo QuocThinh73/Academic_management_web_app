2ac58b52cd3a63ce70158924a4a085c11ff30ead
Zackerville <tien.ngozack2004@hcmut.edu.vn>
2024-04-18 13:19:26 +0700

    update

--- Project/Login/admin.py
+++ Project/Login/admin.py
@@ -2,5 +2,3 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.StudentUser)
-admin.site.register(models.TeacherUser)
--- Project/Login/models.py
+++ Project/Login/models.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class MyUser(AbstractUser):
-    pass
+     USER_TYPE_CHOICES = [
+         ('Student', "Student"),
+         ('Teacher', 'Teacher'),
+     ] 
+     user_type = models.CharField(max_length = 20, choices = USER_TYPE_CHOICES)
 
 class StudentUser(MyUser):
     student_id = models.CharField(max_length=30)
-    user_type = "Student"
+    score = models.FloatField(default = 0.0)
+    date_of_birth = models.DateField(null = True, blank = True)
+    major = models.CharField(max_length = 50, null = True, blank = True)
 
 class TeacherUser(MyUser):
     teacher_id = models.CharField(max_length=30)
-    user_type = "Teacher"
