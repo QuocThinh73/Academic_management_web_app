e585ff1ca6965215b8521e7feecd74586e5905d2
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-18 11:13:53 +0700

    Tao StudentUser va TeacherUser

--- Project/Login/urls.py
+++ Project/Login/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='Login'
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("Login/", views.LoginView.as_view(), name="home")
 ]
