677941fc75eaebf8d2cb4972f357d744e176e8cf
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-20 11:29:49 +0700

    Test môn học

--- Project/Student/models.py
+++ Project/Student/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from Login.models import MyUser
 
 # Create your models here.
 
-class StudentUser(models.Model):
-    student_id = models.CharField(max_length=30, null=True, default='')
-    date_of_birth = models.DateField(null = True, blank = True)
-    major = models.CharField(max_length = 50, null = True, blank = True)
-    enrollment_date = models.DateField(null = True, blank = True)
+class Student(models.Model):
+    id = models.CharField(max_length=15, null=False, primary_key=True, default='')
+    name = models.CharField(max_length=30, null=True, default='')
+    date_of_birth = models.DateField(null = True, default='')
+    department = models.CharField(max_length = 50, null = True, default='')
+    major = models.CharField(max_length = 50, null = True, default='')
+    enrollment_date = models.DateField(null = True, default='')
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
