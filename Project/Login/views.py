51845b4b9776daeee436f01ca83c5d36b23e36ec
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-20 17:47:03 +0700

    Thay url

--- Project/Login/views.py
+++ Project/Login/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse
 from django.views import View
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 
 # Create your views here.
@@ -18,7 +17,7 @@ class LoginView(View):
         matKhau = request.POST.get('password')
         myUser = authenticate(username=tenDangNhap, password=matKhau)
         if myUser is None:
-            return render(request, 'Login/login_fail.html')
+            return HttpResponse("Đăng nhập lỗi")
         else:
             login(request, myUser)
             if myUser.user_type == "Student":
