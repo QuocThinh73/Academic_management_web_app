bb3491e8c42ff7916e69ba903686ea94e1df2e1f
trankiet2004 <kiet111104@gmail.com>
2024-04-26 13:09:54 +0700

    studentview

--- Project/Student/views.py
+++ Project/Student/views.py
@@ -8,3 +8,9 @@ class StudentView(View):
 class StudentCourse(View):
     def get(self, request):
         return render(request, "Student/my_course.html")
+class StudentProfile(View):
+    def get(self, request):
+        return render(request, "Student/profile.html")
+class StudentRegister(View):
+    def get(self, request):
+        return render(request, "Student/course_registration.html")
