06075b9e809449f0eecb26917cc10766841cc423
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-29 01:34:59 +0700

    Tạo form đăng kí môn học

--- Project/Registration/models.py
+++ Project/Registration/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from Student.models import Student
+from Databases.models import Subject
 
-# Create your models here.
+class RegistrationCourse(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.subject.name
--- Project/Registration/views.py
+++ Project/Registration/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from Databases.models import Subject
+from .forms import RegistrationForm
+from .models import RegistrationCourse
 # Create your views here.
 
 class RegistrationStudent(View):
     def get(self, request):
-        return render(request, "Registration/course_registration.html")
+        subjects = Subject.objects.all()
+        form = RegistrationForm()
+        context = {
+            "form": form,
+        }
+        return render(request, "Registration/course_registration.html", context)
+    
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Lấy dữ liệu từ form
+            subject_id = form.cleaned_data['subject']
+            student = request.user.student
+
+            # Kiểm tra xem sinh viên đã đăng kí môn học này chưa
+            if not RegistrationCourse.objects.filter(student=student, subject_id=subject_id).exists():
+                # Nếu chưa đăng kí, lưu thông tin đăng kí vào database
+                registration = form.save(commit=False)
+                registration.student = student
+                registration.save()
+                return redirect("Registration:registration_student")
+
+        # Nếu form không hợp lệ hoặc sinh viên đã đăng kí môn học, hiển thị lại form
+        return render(request, 'Registration/course_registration.html', {'form': form})
