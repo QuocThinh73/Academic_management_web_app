64dfe677f358bd1cb3a091e636ef10a9e5f2a635
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-30 17:38:36 +0700

    Hiển thị ra các nội dung của Student

--- Project/Student/views.py
+++ Project/Student/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Student
 from Course.models import Course
+from Grade.models import Grade
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class StudentView(View):
@@ -10,21 +12,43 @@ class StudentView(View):
     
 class StudentCourse(View):
     def get(self, request):
-        current_user = request.user
-        student = Student.objects.get(username=current_user)
-        student_courses = student.course_set.all()
+        # Lấy thông tin sinh viên
+        student = request.user.student
+        student_courses = Course.objects.filter(students=student)
+
+        # Sắp xếp lớp học theo kì để hiển thị
+        courses_by_semester = {}
+        for course in student_courses:
+            semester_id = course.semester.semester_id
+            if semester_id not in courses_by_semester:
+                courses_by_semester[semester_id] = []
+            courses_by_semester[semester_id].append(course)
+        courses_by_semester = dict(sorted(courses_by_semester.items(), reverse=True))
+
         context = {
-            'courses': student_courses,
+            'courses_by_semester': courses_by_semester,
         }
         return render(request, "Student/my_course.html", context)
     
 class StudentProfile(View):
     def get(self, request):
-        current_user = request.user
-        student = Student.objects.get(username=current_user)
-        student_email = current_user.email
+        # Lấy thông tin của sinh viên
+        student = request.user.student
+        student_email = request.user.email
+        grades = Grade.objects.filter(student=student)
+
+        # Sắp xếp lại điểm theo kì để hiển thị
+        grades_by_semester = {}
+        for grade in grades:
+            semester_id = grade.course.semester.semester_id
+            if semester_id not in grades_by_semester:
+                grades_by_semester[semester_id] = []
+            grades_by_semester[semester_id].append(grade)
+        grades_by_semester = dict(sorted(grades_by_semester.items(), reverse=True))
+
         context = {
                 "student" : student,
                 "student_email" : student_email,
+                "grades_by_semester": grades_by_semester,
         }
         return render(request, "Student/profile.html", context)
