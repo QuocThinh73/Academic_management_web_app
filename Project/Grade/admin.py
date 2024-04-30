b22ca63daf1ad1c3fa8577752276572fa5ff2d33
QuocThinh73 <ttqthinh2004@gmail.com>
2024-04-30 16:19:48 +0700

    Thêm giáo viên cho grade mới tạo

--- Project/Grade/admin.py
+++ Project/Grade/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 from .models import Grade
 # Register your models here.
 
-admin.site.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    def add_teacher_to_grade(self, request, queryset):
+        for grade in queryset:
+            # Có giáo viên rồi thì bỏ qua
+            if grade.teacher:
+                continue
+
+            # Tìm giáo viên quản lí course đó để thêm vào
+            if grade.course.teacher:
+                grade.teacher = grade.course.teacher
+                grade.save()
+                self.message_user(request, f"Đã thêm giáo viên cho {grade} thành công.", level='SUCCESS')
+            else:
+                self.message_user(request, f"Không tìm thấy giáo viên của {grade}.", level='WARNING')
+
+            
+    add_teacher_to_grade.short_description = "Thêm giáo viên"
+
+    actions = ['add_teacher_to_grade']
+
+admin.site.register(Grade, GradeAdmin)
--- Project/Registration/admin.py
+++ Project/Registration/admin.py
@@ -3,44 +3,44 @@ from .models import RegistrationCourse
 from Course.models import Course
 from Grade.models import Grade
 
-def create_course(modeladmin, request, queryset):
-    registrations = queryset.all()
-    
-    # Kiểm tra
-    unique_subjects = set()
-    for registration in registrations:
-        unique_subjects.add(registration.subject)
-    
-    if len(unique_subjects) > 1:
-        modeladmin.message_user(request, "Chỉ có thể chọn các đăng ký cùng một môn học để tạo lớp.", level='ERROR')
-        return
-    
-    first_registration = registrations.first()
-    subject = first_registration.subject
-    semester = first_registration.semester
-    num_courses = Course.count_courses_upon_subject(subject, semester)
-    # Tạo lớp mới
-    course = Course.objects.create(subject=subject, semester=semester, id_course=num_courses + 1)
-
-    # Thêm sinh viên đã đăng kí môn vào lớp
-    for registration in registrations:
-        course.students.add(registration.student)
-
-
-    course.save()
-
-    # Tạo Grade để quản lý điểm
-    for registration in registrations:
-        grade = Grade.objects.create(student=registration.student, course=course)
-        grade.save()
-
-    # Xóa các đơn đăng ký sau khi tạo lớp thành công
-    registrations.delete()
-    modeladmin.message_user(request, "Đã tạo lớp học thành công.", level='SUCCESS')
-
-create_course.short_description = "Tạo lớp học"
-
 class RegistrationCourseAdmin(admin.ModelAdmin):
+    def create_course(modeladmin, request, queryset):
+        registrations = queryset.all()
+    
+        # Kiểm tra
+        unique_subjects = set()
+        for registration in registrations:
+            unique_subjects.add(registration.subject)
+        
+        if len(unique_subjects) > 1:
+            modeladmin.message_user(request, "Chỉ có thể chọn các đăng ký cùng một môn học để tạo lớp.", level='ERROR')
+            return
+        
+        first_registration = registrations.first()
+        subject = first_registration.subject
+        semester = first_registration.semester
+        num_courses = Course.count_courses_upon_subject(subject, semester)
+        # Tạo lớp mới
+        course = Course.objects.create(subject=subject, semester=semester, id_course=num_courses + 1)
+
+        # Thêm sinh viên đã đăng kí môn vào lớp
+        for registration in registrations:
+            course.students.add(registration.student)
+
+
+        course.save()
+
+        # Tạo Grade để quản lý điểm
+        for registration in registrations:
+            grade = Grade.objects.create(student=registration.student, course=course)
+            grade.save()
+
+        # Xóa các đơn đăng ký sau khi tạo lớp thành công
+        registrations.delete()
+        modeladmin.message_user(request, "Đã tạo lớp học thành công.", level='SUCCESS')
+
+    create_course.short_description = "Tạo lớp học"
+
     list_display = ['subject', 'student', 'semester']
     actions = [create_course]
 
