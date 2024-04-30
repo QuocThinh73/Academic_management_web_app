00dc3ad8d8437fca9a7cd98c5e4ab89b7b80e968
QuocThinh73 <ttqthinh2004@gmail.com>
2024-05-01 05:55:47 +0700

    Tính điểm trung bình

--- Project/Grade/models.py
+++ Project/Grade/models.py
@@ -11,6 +11,16 @@ class Grade(models.Model):
     midterm_score = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     final_score = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
 
+    @property
+    def average_score(self):
+        if self.assignment_score is not None and self.midterm_score is not None and self.final_score is not None:
+            assignment_score = float(self.assignment_score)
+            midterm_score = float(self.midterm_score)
+            final_score = float(self.final_score)
+            return round((assignment_score * 0.2 + midterm_score * 0.3 + final_score * 0.5), 2)
+        else:
+            return None
+    
     def __str__(self):
         name = self.course.subject.name + " HK" + self.course.semester.semester_id
         return f"{self.student.name} - {name}"
