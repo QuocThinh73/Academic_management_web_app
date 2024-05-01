633c4d74cd88f5066f0024bcccfab65bf130dc1e
QuocThinh73 <ttqthinh2004@gmail.com>
2024-05-01 23:50:36 +0700

    Hiển thị điểm của sinh viên trong lớp học

--- Project/Course/urls.py
+++ Project/Course/urls.py
@@ -5,6 +5,7 @@ app_name='Course'
 urlpatterns = [
     path("student/<int:course_id>/", views.CourseStudent.as_view(), name="CourseStudent"),
     path("student/<int:course_id>/score_view/", views.ScoreView.as_view(), name="ScoreView"),
+    path("student/<int:course_id>/document_view/", views.DocumentView.as_view(), name="DocumentView"),
     path("teacher/<int:course_id>/", views.CourseTeacher.as_view(), name="CourseTeacher"),
     path("teacher/<int:course_id>/list_of_student/", views.ListOfStudent.as_view(), name="ListOfStudent"),
     path("teacher/<int:course_id>/assessment/", views.Assessment.as_view(), name="Assessment"),
