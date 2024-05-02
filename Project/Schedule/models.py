f10d8179b6180e7432c805dc94c2f1ccaffc400e
QuocThinh73 <ttqthinh2004@gmail.com>
2024-05-02 22:47:06 +0700

    Schedule

--- Project/Schedule/models.py
+++ Project/Schedule/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from Course.models import Course
 # Create your models here.
 
+class DayOfWeek(models.Model):
+    day = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.day
+class HourOfDay(models.Model):
+    hour = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return str(self.hour)
+
 class Schedule(models.Model):
-    course = models.OneToOneField(Course, on_delete=models.CASCADE, null=True)
-    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    days = models.ForeignKey(DayOfWeek, on_delete=models.CASCADE, null=True)
+    hours = models.ForeignKey(HourOfDay, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.course
