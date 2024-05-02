75d8c6c37bc6d50baa5c965354f55c793d0dd7a0
QuocThinh73 <ttqthinh2004@gmail.com>
2024-05-02 23:55:30 +0700

    Xong sắp xếp lịch cơ bản

--- Project/Schedule/models.py
+++ Project/Schedule/models.py
@@ -3,12 +3,12 @@ from Course.models import Course
 # Create your models here.
 
 class DayOfWeek(models.Model):
-    day = models.CharField(max_length=3, unique=True)
+    day = models.CharField(max_length=3, unique=True) # Từ thứ 2 đến thứ 7
 
     def __str__(self):
         return self.day
 class HourOfDay(models.Model):
-    hour = models.IntegerField(unique=True)
+    hour = models.IntegerField(unique=True) # Từ 7h đến 15h
 
     def __str__(self):
         return str(self.hour)
@@ -16,7 +16,13 @@ class HourOfDay(models.Model):
 class Schedule(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     days = models.ForeignKey(DayOfWeek, on_delete=models.CASCADE, null=True)
-    hours = models.ForeignKey(HourOfDay, on_delete=models.CASCADE, null=True)
+    start_hour = models.ForeignKey(HourOfDay, on_delete=models.CASCADE, null=True)
+    end_hour = 0
+
+    def save(self, *args, **kwargs):
+        if self.start_hour:
+            self.end_hour = self.start_hour.hour + self.course.subject.hours
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.course
+        return f"{self.days} - {self.start_hour} - {self.start_hour.hour + self.course.subject.hours}"
