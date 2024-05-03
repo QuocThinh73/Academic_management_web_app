4b64238a47f0add546e27b3532e4c36e1033b58b
trankiet2004 <kiet111104@gmail.com>
2024-05-03 23:45:28 +0700

    da up duoc tai lieu len

--- Project/File_document/forms.py
+++ Project/File_document/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from .models import Document
+from django.forms import modelformset_factory
+
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ['description', 'syllabus', 'course_file']
+        
+DocumentFormSet = modelformset_factory(
+    Document,
+    form=DocumentForm,
+    extra=1,  # Số forms trống để hiển thị mặc định
+    can_delete=True  # Cho phép xóa các tài liệu thông qua formset
+)
--- Project/File_document/views.py
+++ Project/File_document/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Document
 from .forms import DocumentForm
+from .forms import DocumentFormSet
 from Course.models import Course
 from django.views import View
 from Login.mixins import RoleRequiredMixin
@@ -11,22 +12,17 @@ class AddDescription(RoleRequiredMixin, View):
     
     def get(self, request, course_id):
         course = Course.objects.get(pk=course_id)
-        documents = Document.objects.filter(course = course)
-        form = DocumentForm( instance=documents)
-        context = {
-        "course": course,
-        "form": form,
-        }
-        return render(request, "Course/CourseTeacher/course_info.html", context)
+        formset = DocumentFormSet(queryset=Document.objects.filter(course=course))
+        return render(request, "Course/CourseTeacher/course_info.html",  {'formset': formset, 'course': course})
     
     def post(self, request, course_id):
         course = Course.objects.get(pk=course_id)
-        form = DocumentForm(request.POST, prefix=course_id)
-        if form.is_valid():
-            document, created = Document.objects.get_or_create(course = course)
-            if created is False:
-                document.description = form.cleaned_data.get('description')
-                document.syllabus = form.cleaned_data.get('syllabus')
-                document.course_file = form.cleaned_data.get('course_file')
-                document.save()
-        return redirect("File_document:AddDescription", course_id=course_id)
+        formset = DocumentFormSet(request.POST, request.FILES, queryset=Document.objects.filter(course=course))
+        if formset.is_valid():
+            instances = formset.save(commit=False)
+            for instance in instances:
+                instance.course = course
+                instance.save()
+            formset.save_m2m()
+            return redirect("File_document:AddDescription", course_id=course_id)
+        return render(request, "Course/CourseTeacher/course_info.html",  {'formset': formset, 'course': course})
